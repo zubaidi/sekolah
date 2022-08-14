00f6d0152754ffd6601f4c3c67667f278d3c3e28
Muhammad Zubaidi <muhammadzubaidi333@gmail.com>
2022-08-14 14:44:09 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -19,7 +19,12 @@ $queryString = [];
 $rawBody = file_get_contents('php://input', 'r');
 $body = json_decode($rawBody, true);
 
-var_dump($_SERVER['REQUEST_URI']);
+function getNIS(){
+    if(!strpos($_SERVER['REQUEST_URI'], '?')) return false;
+    $path = explode('?', $_SERVER['REQUEST_URI'])[1];
+    $segment = explode('/', $path);
+    return +$segment[1];
+}
 
 switch (strtolower($_SERVER['REQUEST_METHOD'])){
     case 'post':
@@ -52,29 +57,81 @@ switch (strtolower($_SERVER['REQUEST_METHOD'])){
         }
         break;
     case 'get':
-        $stmt = $db->query('SELECT s.NIS, s.NAMA_SISWA, j.NAMA_JURUSAN AS NAMA_JURUSAN, w.NAMA_WALI_KELAS AS NAMA_WALI 
+        $nis = getNIS();
+        if($nis){
+            $stmt = $db->prepare('SELECT * FROM siswa WHERE NIS = :nis');
+            $stmt -> execute([':nis' => $nis]);
+            $data = $stmt->fetch(PDO::FETCH_ASSOC);
+        }else{
+            $stmt = $db->query('SELECT s.NIS, s.NAMA_SISWA, j.NAMA_JURUSAN AS NAMA_JURUSAN, w.NAMA_WALI_KELAS AS NAMA_WALI 
                     FROM siswa s
                     JOIN jurusan j on j.ID_JURUSAN = s.ID_JURUSAN 
                     JOIN walikelas w on w.KD_WALI = s.KD_WALI');
-        $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
+            $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
         if(!empty($data)){
             $RESPON['status'] = 'success';
             // $RESPON['data'] = $data;
-            $RESPON['data'] = [];
+            $RESPON['data'] = $data;
 
-            foreach ($data as $i => $row){
-                $RESPON['data'][$i] = [
-                    'nis' => $row['NIS'],
-                    'nama_siswa' => $row['NAMA_SISWA'],
-                    'nama_jurusan' => $row['NAMA_JURUSAN'],
-                    'nama_walikelas' => $row['NAMA_WALI'],
-                ];
-            }
-
+                // foreach ($data as $i => $row){
+                //     $RESPON['data'][$i] = [
+                //         'nis' => $row['NIS'],
+                //         'nama_siswa' => $row['NAMA_SISWA'],
+                //         'nama_jurusan' => $row['NAMA_JURUSAN'],
+                //         'nama_walikelas' => $row['NAMA_WALI'],
+                //     ];
+                // }
         }else{
             http_response_code(404);
         }
+        break;
+    case 'delete':
+        $nis = getNIS();
+        if(!$nis){
+            http_response_code(404);
+            break;
+        }
+        $stmt = $db->prepare('SELECT * FROM siswa WHERE NIS = :nis');
+        $stmt->bindParam(':nis', $nis, PDO::PARAM_INT);
+        $stmt -> execute();
+
+        if(!$stmt -> rowCount()){
+            http_response_code(404);
+            break;
+        }
+
+        $stmt = $db->prepare('DELETE FROM siswa WHERE NIS = :nis');
+        $stmt->execute([':nis' => $nis]);
+        http_response_code(204);
+        break;
+    case 'put':
+        $nis = getNIS();
+        if(!$nis){}
+        
+        if (!isset($body['nama_siswa'], $body['id_jurusan'], $body['id_walikelas'])){
+            throw new InvalidArgumentException('Invalid Form');
+        }
+        
+        $stmt = $db->prepare('SELECT * FROM siswa WHERE NIS = :nis');
+        $stmt->bindParam(':nis', $nis, PDO::PARAM_INT);
+
+        if($stmt->rowCount()){
+            http_response_code(404);
+            break;
+        }
+        // eksekusi database update
+        $stmt = $db->prepare('UPDATE siswa SET NAMA_SISWA=:nama, ID_JURUSAN=:id_j, KD_WALI=:id_w 
+                                WHERE NIS = :nis');
+        $stmt -> execute([
+            ':nis' => $nis,
+            ':nama' => $body['nama_siswa'],
+            ':id_j' => $body['id_jurusan'],
+            ':id_w' => $body['id_walikelas'],
+        ]);
+        http_response_code(201);
+        $RESPON['status'] = 'success';
+        $RESPON['data'] = 'berhasil diubah';
         break;
     default:
         http_response_code(503);
